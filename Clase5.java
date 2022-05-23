678af691d8da48c36bb2b9413f698716b72297fb
bjcastillo3 <104603220+bjcastillo3@users.noreply.github.com>
2022-05-22 23:21:54 -0500

    Add files via upload

---
+++ Clase5.java
@@ -0,0 +1,8 @@
+/**
+ * Class Untitled
+ */
+public class Clase5 {
+	public static void main(String[] args) {
+    	System.out.println("Hello from AWS Cloud9! into Clase5");
+	}
+}
---
+++ HelloUtplTec.java
@@ -0,0 +1,10 @@
+/**
+ * Class Untitled
+ */
+public class HelloUtplTec {
+	
+	public static void main(String[] args) {
+    	System.out.println("Hello UtplTec from AWS Cloud9!");
+    	
+	}
+}
